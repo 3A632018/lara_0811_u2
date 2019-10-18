8c8a7871cbd4966ae1a84c3eeeb16751e691ec92
3A632018 <f223145@gmail.com>
2019-10-18 18:53:53 +0800

    練習2-1:修改 Route 接受參數

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 //1.設定 Route 回傳字串
-//Route::get('/',function(){
+Route::get('/',function(){
     //return'welcome';
-//});
+});
 
 //2. 設定 Route 回傳 view
 //Route::get('/',function(){
@@ -11,7 +11,12 @@
 //});
 
 //3. 設定 Route 跳轉頁面
-Route::get('/',function(){
-    return redirect('welcome');
-});
+//Route::get('/',function(){
+    //return redirect('welcome');
+//});
 
+
+//2-1. 修改 Route 接受參數
+Route::get('hello/{name}',function($name){
+    return'Hello,'.$name;
+});
