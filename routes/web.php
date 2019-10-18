81814d2b87e61097aff227ffa764f41e2408ea9b
3A632018 <f223145@gmail.com>
2019-10-18 18:51:21 +0800

    練習1-2設定 Route 回傳 view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 //1.設定 Route 回傳字串
+//Route::get('/',function(){
+    //return'welcome';
+//});
+
+//2. 設定 Route 回傳 view
 Route::get('/',function(){
-    return'welcome';
+    return view('welcome');
 });
+
