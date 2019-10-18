708e7506da32ebde9987b9dde3c82ad744fb801e
3A632018 <f223145@gmail.com>
2019-10-18 19:01:49 +0800

    練習4-1:將 Route 取名為 hello.index

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 //1-1.設定 Route 回傳字串
-//Route::get('/',function(){
-    //return'welcome';
-//});
+Route::get('/',function(){
+    return'welcome';
+});
 
 //2. 設定 Route 回傳 view
 //Route::get('/',function(){
@@ -22,6 +22,11 @@
 //});
 
 //3. 修改參數成選擇性
-Route::get('hello/{name?}',function($name='Everybody'){
-return'Hello,'.$name;
-});
+//Route::get('hello/{name?}',function($name='Everybody'){
+//return'Hello,'.$name;
+//});
+
+//4-1. 將 Route 取名為 hello.index
+Route::get('hello/{name?}',['as'=>'hello.index',function($name= 'Everybody'){
+    return'Hello,'.$name;
+}]);
