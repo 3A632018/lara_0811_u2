4827c36dac0ba1de0a5487c012127c1823160cbb
3A632018 <f223145@gmail.com>
2019-10-18 18:59:42 +0800

    練習 3：使用 artisan 指令了解目前 route 內容

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-//1.設定 Route 回傳字串
-Route::get('/',function(){
+//1-1.設定 Route 回傳字串
+//Route::get('/',function(){
     //return'welcome';
-});
+//});
 
 //2. 設定 Route 回傳 view
 //Route::get('/',function(){
@@ -13,7 +13,7 @@ Route::get('/',function(){
 //3. 設定 Route 跳轉頁面
 //Route::get('/',function(){
     //return redirect('welcome');
-//});
+//);
 
 
 //2-1. 修改 Route 接受參數
