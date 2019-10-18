abe3d69c385c27cda66e46b5d970a9fddfc83db2
3A632018 <f223145@gmail.com>
2019-10-18 19:04:48 +0800

    練習4-2修改 resources/views/welcome.blade.php的內容，增加一個連結

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
 //1-1.設定 Route 回傳字串
-Route::get('/',function(){
-    return'welcome';
-});
+//Route::get('/',function(){
+    //return'welcome';
+//});
 
 //2. 設定 Route 回傳 view
-//Route::get('/',function(){
-    //return view('welcome');
-//});
+Route::get('/',function(){
+    return view('welcome');
+});
 
 //3. 設定 Route 跳轉頁面
 //Route::get('/',function(){
